c5365720df3051ddf8eb9ccfd1e024253c67c323
Cecília Lima <cecilia.lima@sou.unifal-mg.edu.br>
2026-04-07 16:05:48 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export'
+
+  typescript: {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
